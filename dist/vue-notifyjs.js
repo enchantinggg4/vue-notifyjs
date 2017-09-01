86bf3c87352e3784e6a27099ac917426f1ae7c5d
enchanting <enhantinggg4@gmail.com>
2017-09-01 17:07:50 +0300

    version update

--- dist/vue-notifyjs.js
+++ dist/vue-notifyjs.js
@@ -268,7 +268,7 @@ var NotificationsPlugin = {
 
             methods: {
                 notify: function notify(notification) {
-                    this.notificationStore.notify(notification);
+                    return this.notificationStore.notify(notification);
                 }
             }
         });
